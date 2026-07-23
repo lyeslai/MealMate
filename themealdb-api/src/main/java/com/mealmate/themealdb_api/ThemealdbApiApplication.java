7ed22dcd2787adc1e3bfe26937280002b598cf4e
lyeslai <youyoulaimouche@gmail.com>
2026-07-23 18:48:54 +0200

    Initial commit

---
+++ themealdb-api/src/main/java/com/mealmate/themealdb_api/ThemealdbApiApplication.java
@@ -0,0 +1,13 @@
+package com.mealmate.themealdb_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemealdbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemealdbApiApplication.class, args);
+	}
+
+}
---
+++ themealdb-api/src/test/java/com/mealmate/themealdb_api/ThemealdbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealmate.themealdb_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemealdbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
