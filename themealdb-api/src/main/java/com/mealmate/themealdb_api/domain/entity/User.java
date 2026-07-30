88a54f24c47a347fe85085e0569b3ccfebd83c11
lyeslai <youyoulaimouche@gmail.com>
2026-07-30 22:05:18 +0200

    Phase 2 : Debut partie user et auth

---
+++ themealdb-api/src/main/java/com/mealmate/themealdb_api/domain/entity/User.java
@@ -0,0 +1,51 @@
+package com.mealmate.themealdb_api.domain.entity;
+
+import java.time.LocalDateTime;
+
+import com.mealmate.themealdb_api.domain.enums.TagRegime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column(nullable = false, unique = true)
+    private String password;
+
+    @Column(nullable = false)
+    private String pseudo;
+
+    @Enumerated(EnumType.STRING)
+    private TagRegime tagsRegime;
+    
+    @Column(nullable = false, updatable = false) 
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
